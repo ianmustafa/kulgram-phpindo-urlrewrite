133d3091eac73b73b19137acb698cf141eaf1298
Ian Mustafa <cuplizian@gmail.com>
2017-02-08 20:45:09 +0700

    Tambahkan make-slug.php untuk membuat slug dengan cepat

---
+++ make-slug.php
@@ -0,0 +1,24 @@
+<?php
+// Import
+$config = require ROOT . 'system/config.php';
+$pdo = require ROOT . 'system/db.php';
+
+// Ambil artikel
+$query = $pdo->query('SELECT id, judul FROM artikel');
+
+// Prepare statement
+$stmt = $pdo->prepare('UPDATE artikel SET slug = :slug WHERE id = :id');
+
+// Loop
+foreach ($query->fetchAll() as $row) {
+    echo "Update artikel #{$row->id}... ";
+
+    // Buat slug dari judul dengan me-replace spasinya menjadi dash
+    // dan mengecilkan huruf kapital
+    $slug = strtolower(str_replace(' ', '-', $row->judul));
+
+    // Eksekusi
+    $stmt->execute(['id' => $row->id, 'slug' => $slug]);
+
+    echo "selesai.<br>" . PHP_EOL;
+}
